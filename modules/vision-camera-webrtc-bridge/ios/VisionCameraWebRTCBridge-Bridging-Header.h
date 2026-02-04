6166ece585658eaa59b3cff9d58fd83bb794b378
Doug G <douglaswgerhardt@gmail.com>
2026-02-04 10:44:33 -0500

    Remove bridging header that broke CocoaPods module build

--- modules/vision-camera-webrtc-bridge/ios/VisionCameraWebRTCBridge-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-// Bridging header for VisionCameraWebRTCBridge
-// Allows Obj-C frame processor plugin to call Swift singleton
-//
-
-#import <VisionCamera/FrameProcessorPlugin.h>
-#import <VisionCamera/FrameProcessorPluginRegistry.h>
-#import <VisionCamera/Frame.h>
